8e205eeac65c58344af3c97e6f8a1b60f51706f7
srini akhil <sriniakhil13@gmail.com>
2020-03-28 15:03:51 +0530

    speechtotext python file

---
+++ speech2text.py
@@ -0,0 +1,28 @@
+import azure.cognitiveservices.speech as speechsdk
+
+#put your region and subscription key here
+speech_config = speechsdk.SpeechConfig(subscription='', region="")
+
+# Creates an audio configuration that points to an audio file.
+# Replace with your own audio filename.
+audio_filename = "" #path to audio file 
+audio_input = speechsdk.AudioConfig(filename=audio_filename)
+
+# Creates a recognizer with the given settings
+speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+
+print("Recognizing first result...")
+
+
+result = speech_recognizer.recognize_once()
+
+# Checks result.
+if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+    print("Recognized: {}".format(result.text))
+elif result.reason == speechsdk.ResultReason.NoMatch:
+    print("No speech could be recognized: {}".format(result.no_match_details))
+elif result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = result.cancellation_details
+    print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        print("Error details: {}".format(cancellation_details.error_details))
